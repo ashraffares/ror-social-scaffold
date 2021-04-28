30377faa1331b7649a182c76eceb7fd3f818b8d2
Fares <ashraffares090@gmail.com>
2021-04-28 22:09:14 +0200

    Add create and destroy method in friendships controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,4 +2,17 @@ class FriendshipsController < ApplicationController
     def new 
         @friendship = Friendship.new
     end
+
+    def create 
+      @friendship = current_user.friendships.build(friend_id: params[:user_id])
+      redirect_to users_path if @friendship.save
+      redirect_to users_path, notice: 'Something Went Wrong Try Again Later'
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @friendship = current_user.inverse_friendships.find { |friendship| friendship.user == @user }
+        @friendship.destroy
+        redirect_to users_path, notice: 'successfully removed'
+    end
 end
