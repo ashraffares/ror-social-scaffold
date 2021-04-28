8500b9008563cb485258e79469ebde46431ab851
Fares <ashraffares090@gmail.com>
2021-04-28 22:27:01 +0200

    Add Friends methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,22 @@ class User < ApplicationRecord
 
   has_many :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+
+  def pending_friends
+    friendships.map{ |friendship| friendship.friend unless friendship.confirmed}.compact
+  end
+
+  def friend_requests
+    inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact
+  end
+
+  def confirm_friend(user)
+    friendship = inverse_friendships.find{|friendship| friendship.user == user}
+    friendship.confirmed = true
+    friendship.save
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
 end
