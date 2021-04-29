69825fb3a5c96f9ef01fd4b2c4acaabaa068df5f
Fares <ashraffares090@gmail.com>
2021-04-29 19:37:00 +0200

    Add linke to create friend_request

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,18 +1,18 @@
 class FriendshipsController < ApplicationController
-    def new 
-        @friendship = Friendship.new
-    end
+  def create
+    @friendship = current_user.friendships.build(friend_id: params[:user_id])
+    return redirect_to users_path, notice: 'Successfully sent' if @friendship.save
+    redirect_to users_path, notice: 'Something Went Wrong Please Try Again Later'
+  end
 
-    def create 
-      @friendship = current_user.friendships.build(friend_id: params[:user_id])
-      redirect_to users_path if @friendship.save
-      redirect_to users_path, notice: 'Something Went Wrong Try Again Later'
-    end
+  def destroy
+    @user = User.find(params[:id])
+    @friendship = current_user.inverse_friendships.find { |friendship| friendship.user == @user }
+    @friendship.destroy
+    redirect_to users_path, notice: 'successfully removed'
+  end
 
-    def destroy
-        @user = User.find(params[:id])
-        @friendship = current_user.inverse_friendships.find { |friendship| friendship.user == @user }
-        @friendship.destroy
-        redirect_to users_path, notice: 'successfully removed'
-    end
+  def user
+    @friend = User.find(params[:user_id])
+  end
 end
