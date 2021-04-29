73f33f8e976a71f94b3a9811e8776f3efaad4f26
Fares <ashraffares090@gmail.com>
2021-04-29 22:35:05 +0200

    hide link_to friend when user on his show page

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,8 +1,11 @@
 class FriendshipsController < ApplicationController
   def create
     @friendship = current_user.friendships.build(friend_id: params[:user_id])
-    return redirect_to users_path, notice: 'Successfully sent' if @friendship.save
-    redirect_to users_path, notice: 'Something Went Wrong Please Try Again Later'
+    if @friendship.save
+      redirect_to users_path, notice: 'Successfully sent'
+    else
+      redirect_to users_path, notice: 'Something Went Wrong Please Try Again Later'
+    end
   end
 
   def destroy
@@ -10,9 +13,5 @@ class FriendshipsController < ApplicationController
     @friendship = current_user.inverse_friendships.find { |friendship| friendship.user == @user }
     @friendship.destroy
     redirect_to users_path, notice: 'successfully removed'
-  end
-
-  def user
-    @friend = User.find(params[:user_id])
   end
 end
