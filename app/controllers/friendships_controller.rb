bbdb8c9a46ec15e5446cb0de272717d00367d6b1
Fares <ashraffares090@gmail.com>
2021-05-01 10:57:20 +0200

    Fix rubocop indentation error

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,6 @@
 class FriendshipsController < ApplicationController
   def create
-    @friendship = current_user.friendships.build(friend_id: params[:user_id],confirmed:false)
+    @friendship = current_user.friendships.build(friend_id: params[:user_id], confirmed: false)
     if @friendship.save
       redirect_to users_path, notice: 'Successfully sent request'
     else
